7bb980b57d317bc809414f79a6e658a522be896e
Kailash <k11pogra@gmail.com>
2023-08-12 09:52:02 +0530

    loops

---
+++ src/loops.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class loops {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+      // int n = sc.nextInt();
+        // print the sum of n natural numbers , where n is the input
+//        int sum = 0;
+//        int i = 0;
+//            while(i<n){
+//                i++;
+//                sum=sum+i;
+//            }
+//        System.out.println(sum);
+
+// solve same question using for loop
+//        int sum = 0;
+//        for (int num = 0 ;num<=n; num++){
+//            sum = sum+num;
+//            // num++ is happen just before last semicolon
+//        }
+//        System.out.println(sum);
+
+ // print the input number in reverse order
+//
+//        for (int i=n ; i>=1; i-- ){
+//            System.out.println(i);
+//        }
+
+        // do while loop
+        //in this loop first itration always execute then it check condition
+       int num = 1;
+        do {
+            System.out.println(num);
+            num++;
+        }while (num<=10);
+    }
+}
